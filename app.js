a8e709faef43e5be79474e73ab172c2ddb5abf91
Mohamed <mohamed.azd49@gmail.com>
2023-05-17 18:41:09 +0200

    ajout de cors

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ import express from "express";
 import routes from "./routes/routes.js";
 import db from "./db.js";
 import {} from "./middlewares/auth.js"
+import cors from 'cors'
 
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 app.use(routes);
 
 db.sync()
