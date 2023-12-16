48b23a2a2dca7dc86a36bc5e6464646df7f4307d
Mohamed <mohamed.azd49@gmail.com>
2023-12-16 16:53:25 +0100

    menu

---
+++ front/src/components/MyMenu.jsx
@@ -0,0 +1,22 @@
+import { Button, Menu, MenuButton, MenuDivider, MenuGroup, MenuItem, MenuList } from "@chakra-ui/react";
+
+export default function MyMenu() {
+  return (
+    <Menu closeOnSelect={true}>
+      <MenuButton as={Button} bgColor={"primary"} leftIcon={<span className="material-symbols-outlined">menu</span>}>
+        Menu
+      </MenuButton>
+      <MenuList bgColor={"secondary"}>
+        <MenuGroup title="Client">
+          <MenuItem bgColor={"secondary"} icon={<span className="material-symbols-outlined">add</span>}>Ajouter</MenuItem>
+          <MenuItem bgColor={"secondary"} icon={<span className="material-symbols-outlined">delete</span>}>Supprimer</MenuItem>
+        </MenuGroup>
+        <MenuDivider />
+        <MenuGroup title="Profil">
+          <MenuItem bgColor={"secondary"} icon={<span className="material-symbols-outlined">person</span>}>Mon compte</MenuItem>
+          <MenuItem bgColor={"secondary"} textColor={"red"} icon={<span className="material-symbols-outlined">logout</span>}>Se déconnecter</MenuItem>
+        </MenuGroup>
+      </MenuList>
+    </Menu>
+  );
+}
--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Title from "../components/title";
 import ClientService from "../services/ClientService";
 import ClientList from "../components/clientList";
+import MyMenu from "../components/MyMenu";
 
 export default function Home() {
   const [clients, setClients] = useState([]);
@@ -9,7 +10,7 @@ export default function Home() {
     const jwt = localStorage.getItem("accessToken");
     const clientService = new ClientService(jwt);
     const data = await clientService.getAll();
-    if(!data) console.log("error fetching data")
+    if (!data) console.log("error fetching data");
     setClients(data);
   }
 
@@ -18,16 +19,21 @@ export default function Home() {
   }, []);
 
   return (
-    <div id="home-page" className="h-screen flex flex-col items-center">
-      <div id="title-container" className="h-1/4 pt-5">
-        <Title></Title>
+    <>
+      <div id="home-page" className="h-screen flex flex-col items-center">
+      <div id="menu-container" className="fixed top-6 left-6 right-0">
+        <MyMenu></MyMenu>
       </div>
+        <div id="title-container" className="h-1/4 pt-5">
+          <Title></Title>
+        </div>
 
-      <div id="clients-container" className="w-full flex justify-center mt-5">
-        <div id="clients" className="w-1/3 grid grid-cols-3 gap-x-5 gap-y-5 items-center">
-          <ClientList clients={clients} />
+        <div id="clients-container" className="w-full flex justify-center mt-5">
+          <div id="clients" className="w-1/3 grid grid-cols-3 gap-x-5 gap-y-5 items-center">
+            <ClientList clients={clients} />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
